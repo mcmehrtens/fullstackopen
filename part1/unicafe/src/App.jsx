477643712b551f6f7fdfb5575846de302be7afc5
Matthew Mehrtens <matthew@mehrtens.cc>
2023-12-31 21:08:08 +0100

    Completed exercise 1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -8,14 +8,20 @@ const Button = ({handleClick, text}) => (
     </button>
 )
 
-const Statistic = ({name, value}) => (
-    <p>{name} {value}</p>
-)
+const Statistic = ({name, value, suffix}) => {
+    if (isNaN(value)) {
+        return (<p>{name} N/A</p>)
+    }
+    return (<p>{name} {value}{suffix}</p>)
+}
 
 const App = () => {
     const [good, setGood] = useState(0)
     const [neutral, setNeutral] = useState(0)
     const [bad, setBad] = useState(0)
+    const total = good + neutral + bad
+    const average = (good - bad) / total
+    const percentPositive = good / total * 100
 
     const handleClick = ({value, setValue}) => () => setValue(value + 1)
 
@@ -44,7 +50,9 @@ const App = () => {
             <Statistic name={'good'} value={good}/>
             <Statistic name={'neutral'} value={neutral}/>
             <Statistic name={'bad'} value={bad}/>
-
+            <Statistic name={'all'} value={total}/>
+            <Statistic name={'average'} value={average}/>
+            <Statistic name={'positive'} value={percentPositive} suffix={' %'}/>
         </>
     )
 }
