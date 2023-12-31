264d92021cb5d2ebf397f47b24ff131ff0dec927
Matthew Mehrtens <matthew@mehrtens.cc>
2023-12-31 21:24:04 +0100

    Completed exercise 1.8

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,7 +1,5 @@
 import {useState} from 'react'
 
-const Section = ({title}) => <h1>{title}</h1>
-
 const Button = ({handleClick, text}) => (
     <button onClick={handleClick}>
         {text}
@@ -15,44 +13,55 @@ const Statistic = ({name, value, suffix}) => {
     return (<p>{name} {value}{suffix}</p>)
 }
 
-const App = () => {
-    const [good, setGood] = useState(0)
-    const [neutral, setNeutral] = useState(0)
-    const [bad, setBad] = useState(0)
+const Statistics = ({good, neutral, bad}) => {
     const total = good + neutral + bad
     const average = (good - bad) / total
     const percentPositive = good / total * 100
 
-    const handleClick = ({value, setValue}) => () => setValue(value + 1)
-
     return (
-        <>
-            <Section title={'give feedback'}/>
-            <Button
-                handleClick={handleClick({
-                    value: good,
-                    setValue: setGood})}
-                text={'good'}
-            />
-            <Button
-                handleClick={handleClick({
-                    value: neutral,
-                    setValue: setNeutral})}
-                text={'neutral'}
-            />
-            <Button
-                handleClick={handleClick({
-                    value: bad,
-                    setValue: setBad})}
-                text={'bad'}
-            />
-            <Section title={'statistics'}/>
+        <div>
+            <h1>statistics</h1>
             <Statistic name={'good'} value={good}/>
             <Statistic name={'neutral'} value={neutral}/>
             <Statistic name={'bad'} value={bad}/>
             <Statistic name={'all'} value={total}/>
             <Statistic name={'average'} value={average}/>
             <Statistic name={'positive'} value={percentPositive} suffix={' %'}/>
+        </div>
+    )
+}
+
+const App = () => {
+    const [good, setGood] = useState(0)
+    const [neutral, setNeutral] = useState(0)
+    const [bad, setBad] = useState(0)
+
+    const handleClick = ({value, setValue}) => () => setValue(value + 1)
+
+    return (
+        <>
+            <div>
+                <h1>give feedback</h1>
+                <Button
+                    handleClick={handleClick({
+                        value: good,
+                        setValue: setGood})}
+                    text={'good'}
+                />
+                <Button
+                    handleClick={handleClick({
+                        value: neutral,
+                        setValue: setNeutral})}
+                    text={'neutral'}
+                />
+                <Button
+                    handleClick={handleClick({
+                        value: bad,
+                        setValue: setBad})}
+                    text={'bad'}
+                />
+            </div>
+            <Statistics good={good} neutral={neutral} bad={bad}/>
         </>
     )
 }
