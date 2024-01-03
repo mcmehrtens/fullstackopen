4ec65b33bd71d8b37a198916f0cfda63656e0198
Matthew Mehrtens <matthew@mehrtens.cc>
2024-01-03 14:22:22 +0100

    Completed exercise 2.16

--- part2/The Phonebook/src/App.jsx
+++ part2/The Phonebook/src/App.jsx
@@ -1,6 +1,50 @@
 import {useEffect, useState} from 'react'
 import personService from './services/persons'
 
+const notificationType = {
+    confirm: 'confirm',
+    error: 'error',
+}
+
+const Notification = ({message, type}) => {
+    if (message === null) {
+        return null
+    }
+
+    let notificationStyle;
+
+    switch(type) {
+        case notificationType.confirm:
+            notificationStyle = {
+                textAlign: 'center',
+                backgroundColor: 'rgba(61,167,95,0.65)',
+                color: 'white',
+                marginBottom: 25,
+                borderRadius: 10,
+                padding: 10,
+            }
+            break
+        case notificationType.error:
+            notificationStyle = {
+                textAlign: 'center',
+                backgroundColor: 'rgba(167,11,40,0.75)',
+                color: 'white',
+                marginBottom: 25,
+                borderRadius: 10,
+                padding: 10,
+            }
+            break
+        default:
+            notificationStyle = null
+    }
+
+    return (
+        <div style={notificationStyle}>
+            {message}
+        </div>
+    )
+}
+
 const Input = ({value, handler}) =>
     <input value={value} onChange={(event) => handler(event.target.value)} />
 
@@ -42,6 +86,23 @@ const App = () => {
     const [filter, setFilter] = useState('')
     const [newName, setNewName] = useState('')
     const [newNumber, setNewNumber] = useState('')
+    const [notification, setNotification] = useState({
+        message: null,
+        type: notificationType.confirm,
+    })
+
+    const notifyUser = (message, type) => {
+        setNotification({
+            message: message,
+            type: type,
+        })
+        setTimeout(() => {
+            setNotification({
+                ...notification,
+                message: null,
+            })
+        }, 5000)
+    }
 
     useEffect(() => {
         personService.getAll().then(initialPersons =>
@@ -51,9 +112,14 @@ const App = () => {
     const updatePerson = (id, updatedPerson) => {
         personService
             .update(id, updatedPerson)
-            .then(returnedPerson => setPersons(persons.map(person =>
-                person.id !== returnedPerson.id ? person : returnedPerson
-            )))
+            .then(returnedPerson => {
+                setPersons(persons.map(person =>
+                    person.id !== returnedPerson.id ? person : returnedPerson
+                ))
+                notifyUser(
+                    `Updated phone number for '${returnedPerson.name}'`,
+                    notificationType.confirm)
+            })
     }
 
     const addPerson = event => {
@@ -68,9 +134,13 @@ const App = () => {
                 updatePerson(id, personObject)
             } else return
         } else {
-            personService.add(personObject).then(returnedPerson =>
+            personService.add(personObject).then(returnedPerson => {
                 setPersons(persons.concat(returnedPerson))
-            )
+                notifyUser(
+                    `'${returnedPerson.name}' was added`,
+                    notificationType.confirm
+                )
+            })
         }
         setNewName('')
         setNewNumber('')
@@ -90,6 +160,7 @@ const App = () => {
     return (
         <>
             <h2>Phonebook</h2>
+            <Notification message={notification.message} type={notification.type} />
             <Filter filter={filter} setFilter={setFilter} />
             <h3>add a new</h3>
             <PersonForm addPersonHandler={addPerson} newName={newName}
